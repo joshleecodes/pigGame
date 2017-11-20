18d6c04f82ee6938ee3c13cfd4d814967ff28ad5
Josh Lee <joshlee.dev@gmail.com>
2017-11-20 11:05:54 +0100

    JS Functionality
    Game Initialisation/reset
    Active player toggle
    Button Implementation of roll, hold and new game

--- app.js
+++ app.js
@@ -8,3 +8,84 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, activeGame;
+
+reset();
+
+//dice rolled
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    if(activeGame) {
+        var dice = Math.floor(Math.random()*6) + 1;
+        //display dice roll
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        if(dice !== 1){
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else{ //swap activePlayer
+            nextPlayer();
+        }
+    }
+});
+
+//Player held score
+document.querySelector('.btn-hold').addEventListener('click', function (){
+    if (activeGame){
+        //store score & update UI
+        scores[activePlayer] += roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //check if player won game
+        if (scores[activePlayer] >= 100){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); 
+            activeGame = false;
+        } else {
+            nextPlayer(); //Swap activePlayer
+        }
+    }  
+});
+
+function nextPlayer(){
+    //swap activePlayer
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0; //reset scores
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    //Toggle active player display
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceDOM.style.display = 'none';
+}
+
+function reset(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    activeGame = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+    //Set scores to 0
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    //Reset names
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    //Reset panel classes
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+}
+
+document.querySelector('.btn-new').addEventListener('click', reset);
+
